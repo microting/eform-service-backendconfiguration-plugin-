53da5885aef3638037d262c76d42da4cdcefec9b
René Schultz Madsen <rm@microting.com>
2022-01-18 15:21:55 +0100

    Updating the build script.

--- upgradeeformnugets.sh
+++ upgradeeformnugets.sh
@@ -56,10 +56,6 @@ if (( "$GIT_STATUS" > 0 )); then
 		git push
 		echo "Updated Microting eForm to ${EFORM_VERSION} and pushed new version ${NEW_GIT_VERSION}"
 		cd ..
-		github_changelog_generator -u microting -p $REPOSITORY -t $CHANGELOG_GITHUB_TOKEN
-		git add CHANGELOG.md
-		git commit -m "Updating changelog"
-		git push
 	else
 		echo "nothing to do, everything is up to date."
 	fi
