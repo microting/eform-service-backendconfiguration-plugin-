19e19aef0379ae3f0af61739e63bb79ecabe13d5
René Schultz Madsen <rm@microting.com>
2023-05-17 11:36:19 +0200

    Adding more steps for test for eformparsed in sdk and we pass on the message correctly to the service.

---
+++ ci/eformparsed/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Castle.Windsor;
+using Microting.eForm.Installers;
+using Rebus.Bus;
+
+namespace eformparsed
+{
+    class Program
+    {
+
+        private static IWindsorContainer _container;
+        private static IBus _bus;
+
+        static void Main(string[] args)
+        {
+            var hostname = "localhost";
+
+            Console.WriteLine($"trying to connect to {hostname}");
+
+            _container = new WindsorContainer();
+            _container.Install(
+                new RebusHandlerInstaller()
+                , new RebusInstaller("420", "empty-string", 1, 1, "admin", "password", hostname)
+            );
+            _bus = _container.Resolve<IBus>();
+        }
+    }
+}
