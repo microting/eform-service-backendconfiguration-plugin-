1354453497412fe012e60fe3d7225e7717d81bbe
René Schultz Madsen <rm@microting.com>
2023-06-11 12:18:55 +0200

    Removing the direct dependency of Magick.NET-Q16-x64.

--- upgradeeformnugets.sh
+++ upgradeeformnugets.sh
@@ -7,7 +7,7 @@ if (( "$GIT_STATUS" > 0 )); then
 	cd ServiceBackendConfigurationPlugin
   CURRENT_NUMBER_OF_COMMITS=`git log --oneline | wc -l`
 
-	PACKAGES=('Microting.eForm' 'Microting.eFormApi.BasePn' 'Microting.EformBackendConfigurationBase' 'Magick.NET-Q16-x64' 'Microting.ItemsPlanningBase' 'SendGrid' 'ChemicalsBase' 'Microting.EformAngularFrontendBase' 'Microting.eFormCaseTemplateBase')
+	PACKAGES=('Microting.eForm' 'Microting.eFormApi.BasePn' 'Microting.EformBackendConfigurationBase' 'Microting.ItemsPlanningBase' 'SendGrid' 'ChemicalsBase' 'Microting.EformAngularFrontendBase' 'Microting.eFormCaseTemplateBase')
 	PROJECT_NAME='ServiceBackendConfigurationPlugin.csproj'
 	REPOSITORY='eform-service-backend-configuration-plugin'
 
