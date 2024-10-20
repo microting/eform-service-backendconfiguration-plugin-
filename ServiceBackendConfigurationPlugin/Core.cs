a13af39a2a86080e71204c3be473751141190d1d
René Schultz Madsen <rm@microting.com>
2024-10-20 09:29:05 +0200

    Adding Sentry to catch any errors.

--- ServiceBackendConfigurationPlugin/Core.cs
+++ ServiceBackendConfigurationPlugin/Core.cs
@@ -32,6 +32,7 @@ using Microting.EformAngularFrontendBase.Infrastructure.Data.Factories;
 using Microting.EformBackendConfigurationBase.Infrastructure.Enum;
 using Microting.eFormCaseTemplateBase.Infrastructure.Data.Factories;
 using QuestPDF.Infrastructure;
+using Sentry;
 using ServiceBackendConfigurationPlugin.Resources;
 using ServiceBackendConfigurationPlugin.Scheduler.Jobs;
 
@@ -139,6 +140,41 @@ public class Core : ISdkEventHandler
 
     public bool Start(string sdkConnectionString, string serviceLocation)
     {
+        SentrySdk.Init(options =>
+        {
+            // A Sentry Data Source Name (DSN) is required.
+            // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+            // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
+            options.Dsn = "https://5b67d97e4f88df38b0bbfcd8ed3b9a65@o4506241219428352.ingest.us.sentry.io/4508153878282240";
+
+            // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
+            // This might be helpful, or might interfere with the normal operation of your application.
+            // We enable it here for demonstration purposes when first trying Sentry.
+            // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
+            options.Debug = true;
+
+            // This option is recommended. It enables Sentry's "Release Health" feature.
+            options.AutoSessionTracking = true;
+
+            // Set TracesSampleRate to 1.0 to capture 100%
+            // of transactions for tracing.
+            // We recommend adjusting this value in production.
+            options.TracesSampleRate = 1.0;
+
+            // Sample rate for profiling, applied on top of othe TracesSampleRate,
+            // e.g. 0.2 means we want to profile 20 % of the captured transactions.
+            // We recommend adjusting this value in production.
+            options.ProfilesSampleRate = 1.0;
+            // Requires NuGet package: Sentry.Profiling
+            // Note: By default, the profiler is initialized asynchronously. This can
+            // be tuned by passing a desired initialization timeout to the constructor.
+            // options.AddIntegration(new ProfilingIntegration(
+            //     // During startup, wait up to 500ms to profile the app startup code.
+            //     // This could make launching the app a bit slower so comment it out if you
+            //     // prefer profiling to start asynchronously
+            //     TimeSpan.FromMilliseconds(500)
+            // ));
+        });
         QuestPDF.Settings.License = LicenseType.Community;
 
         Console.WriteLine("ServiceBackendConfigurationPlugin start called");
